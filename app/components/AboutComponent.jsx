171aa35950ecf381e4970b28e246730869d11d74
Aittaleb <hamid.man14@gmail.com>
2017-12-31 11:15:23 +0000

    about page setup

--- app/components/AboutComponent.jsx
+++ app/components/AboutComponent.jsx
@@ -2,9 +2,27 @@ var React = require('react');
 
 var AboutComponent = () => (
     <div>
-        <h2>
-            About component
-        </h2>
+        <h1 className="text-center">
+            About
+        </h1>
+        <p>
+            welcome to the about page <br />
+            this web app is designed in the context of learning the react framework , <br /> feel free to check out my repo
+             on <a href="https://github.com/Aittaleb/react-weather" target="_blank"> github </a>
+            <br />
+            and for further documentation here is link to the api docs i've used
+            <a href="https://openweathermap.org/current" target="_blank"> open weather map</a> <br/> 
+            you can also check on the javascript framework <a href="https://facebook.github.io/react">here</a>
+
+
+            <br/>
+            <br/>
+            <br/>
+
+            ...peace
+            
+        </p>
+
     </div>
 );
 
